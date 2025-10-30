bc9715c2751e0b86968eb7d0ffde46744227d886
Dhennis Nizal <dhnizal@gmail.com>
2025-10-30 19:36:59 +0800

    Initial commit

---
+++ src/styles/index.js
@@ -0,0 +1,17 @@
+export const breakpoint = {
+  mobile: "@media only screen and (max-width: 767px)",
+  tablet: "@media only screen and (min-width: 768px) and (max-width: 1279px)",
+  desktop: "@media only screen and (min-width: 1280px)",
+};
+
+export const color = {
+  transparent: "rgba(0,0,0,0)",
+  white: "#ffffff",
+  black: "#000000",
+};
+
+export const shadow = {
+  thin: "rgba(0, 0, 0, 0.04) 0px 4px 6px -1px,rgba(0, 0, 0, 0.04) 0px 2px 4px -1px", // box
+  soft: "rgba(0, 0, 0, 0.04) 0px 4px 12px", // usually on header
+  medium: "0 4px 8px rgba(0, 0, 0, 0.2)", // hover
+};
