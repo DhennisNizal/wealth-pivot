74edc17aedc9c29fa3f6db8e4742cc89176abd51
Dhennis Nizal <dhnizal@gmail.com>
2025-12-22 13:39:08 +0800

    feat: create article list page

---
+++ src/modules/article-list/artcle-list.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  color: white;
+`;
+
+const ArticleList = () => {
+  return <Title>Article list</Title>;
+};
+
+export default ArticleList;
---
+++ src/pages/article-page.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import ArticleList from "../modules/article-list";
+
+const ArticlePage = () => {
+  return <ArticleList />;
+};
+
+export default ArticlePage;
