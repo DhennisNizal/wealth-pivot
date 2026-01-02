fdfa69340fc1cfe674f14616ef0dba41efbbd0bb
Dhennis Nizal <dhnizal@gmail.com>
2026-01-02 18:19:34 +0800

    feat: deploy to git pages

--- src/route/app-route.jsx
+++ src/route/app-route.jsx
@@ -1,18 +1,19 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import LandingPage from "../pages/landing-page";
 import ArticlePage from "../pages/article-page";
 import ArticleContentPage from "../pages/article-content-page";
 
 const AppRoute = () => {
   return (
-    <Router>
+    <HashRouter>
       <Routes>
         <Route path="/" element={<LandingPage />} />
+        <Route path="/home" element={<LandingPage />} />
         <Route path="/article-page" element={<ArticlePage />} />
         <Route path="/articles/:id" element={<ArticleContentPage />} />
       </Routes>
-    </Router>
+    </HashRouter>
   );
 };
 
